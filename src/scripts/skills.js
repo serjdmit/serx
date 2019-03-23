116d79b2a34c0eacb3d9d13000064dd1f7482abd
Sergei Dmitrijev <mr.dmit@gmail.com>
2019-03-23 17:26:05 +0200

    Begining of working with Vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,54 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+
+
+
+const skill = {
+    template: "#skill",
+    props: {
+        skillName: String,
+        skillPercents: Number
+    },
+    methods: {
+        drawColoredCircle() {
+            const circle = this.$refs["color-circle"];
+
+            const dashArrey = parseInt(
+                getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+            );
+            const percents = (dashArrey / 100) * (100 - this.skillPercents);
+            circle.style.strokeDashoffset = percents;
+        }
+    },
+    mounted() {
+        this.drawColoredCircle();
+    }
+}
+
+const skillsRow = {
+    template: "#skills-row",
+    components: {
+        skill
+    },
+    props: {
+        skill: Object
+    }
+}
+
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-block",
+    components: {
+        skillsRow
+    },
+    data() {
+        return {
+            skills: {}
+        }
+    },
+    created() {
+        const data = require("../data/skills.json");
+        this.skills = data;
+    }
+})
