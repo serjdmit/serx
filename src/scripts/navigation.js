4faafb22179b88bbedb3f7d06c530128beccfb85
Sergei Dmitrijev <mr.dmit@gmail.com>
2019-03-27 18:14:34 +0200

    Made menu Vue template and added hamburger button with animation

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,5 +2,27 @@ import Vue from 'vue';
 
 new Vue ({
     el: "#navigation-component",
-    template: "#navigation-block"
+    template: "#navigation-block",
+    data() {
+        return {
+            items: []
+        }
+    },
+    created() {
+        const data = require('../data/navigation.json');
+        this.items = data;
+    }
+});
+
+new Vue ({
+    el: "#nav-button",
+    template: "#nav-hamburger",
+    data :{
+        isActive: false
+    },
+    methods: {
+        toggleActive() {
+            this.isActive = !this.isActive
+        }
+    }
 });
