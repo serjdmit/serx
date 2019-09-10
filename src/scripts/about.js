f14158985de26ecf05e4c2281a42e7ee28ca8ab5
Sergei Dmitrijev <mr.dmit@gmail.com>
2019-09-10 23:26:18 +0300

    Added text letters animatioin to about section

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,4 +1,7 @@
 import Vue from 'vue';
+import VueWaypoint from 'vue-waypoint';
+
+Vue.use(VueWaypoint);
 
 new Vue({
     el: '#about-component',
@@ -10,7 +13,48 @@ new Vue({
             userName: '',
             greeting: '',
             content: '',
-            subContent: ''
+            subContent: '',
+            typedContent: '',
+            intersectionOptions: {
+                root: null,
+                rootMargin: '0px 0px 0px 0px',
+                thresholds: [0]
+            },
+            visible: false
+        };
+    },
+    methods: {
+        onWaypoint({going}){
+            if (going === this.$waypointMap.GOING_IN) {
+                this.typeText();
+            }
+        },
+        randomNumber(min, max) {
+            return Math.random() * (max - min) + min;
+        },
+        typeLetter(index) {
+            this.typedContent += this.content[index];
+            let interval = this.randomNumber(10, 90);
+            setTimeout(() => {
+                this.typeText();
+            }, interval);
+            if (this.content === this.typedContent) {
+                console.log('asdasd')
+                this.visible = true;
+            }
+        },
+        typeText() {
+            // let i = 0;
+            // while (i < this.content.length) {
+            //     setTimeout(() => {
+            //         this.typedContent += this.content[i];
+            //         console.log(this.typedContent);
+            //         i++;
+            //     }, 5000);
+            // }
+            if (this.content !== this.typedContent) {
+                this.typeLetter(this.typedContent.length);
+            }
         }
     },
     created(){
@@ -21,5 +65,8 @@ new Vue({
         this.greeting = 'Hi, my name is';
         this.content = `I'm FRONT-END developer. I was working as web developer in several big and little companies. I'm freelancer too, sometimes`;
         this.subContent = 'If you need front-end developer, we have only one solution ever: CONTACT ME!';
-    }
+    },
+    // mounted() {
+    //     this.typeText();
+    // }
 });
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import axios from 'axios';
-import VueWaypoint from 'vue-waypoint'
+import VueWaypoint from 'vue-waypoint';
  
-Vue.use(VueWaypoint)
+Vue.use(VueWaypoint);
 
 
 const skill = {
